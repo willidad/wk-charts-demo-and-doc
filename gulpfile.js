4cc2d2ecb4757951445c3db7f882987b67e3ad30
willi <willi@klenk-bondorf.de>
2014-11-03 22:35:24 +0100

    cleaned gulp file and package.json implemented directory structure for build directory

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var buildDir = './build';
 var gulp            = require('gulp');
 var gutil           = require('gulp-util');
 var connect         = require('gulp-connect');
-var gulpif          = require('gulp-if');
 var coffee          = require('gulp-coffee');
 var sourcemaps      = require('gulp-sourcemaps');
 var concat          = require('gulp-concat');
@@ -16,35 +15,34 @@ var markdown        = require('gulp-markdown');
 var es              = require('event-stream');
 var mainBowerFiles  = require('main-bower-files');
 
-console.log(mainBowerFiles({filter:/\.css$/}));
-
 gulp.task('appJS', function() {
   // concatenate compiled .coffee files and js files
   // into build/app.js
     var js = gulp.src(['!./app/**/*_test.js','./app/**/*.js'])
         .pipe(sourcemaps.init());
+
     var csJs = gulp.src(['!./app/**/*_test.coffee','./app/**/*.coffee'])
         .pipe(sourcemaps.init())
         .pipe(coffee({bare:true}).on('error', gutil.log));
+
     es.merge(js,csJs)
     .pipe(concat('app.js'))
-    .pipe(sourcemaps.write('/maps'))
-    .pipe(gulp.dest(buildDir))
+    .pipe(sourcemaps.write('/sourcemaps'))
+    .pipe(gulp.dest(buildDir + '/js'))
 });
 
 gulp.task('testJS', function() {
-  // Compile JS test files. Not compiled.
-  gulp.src([
-      './app/**/*_test.js',
-      './app/**/*_test.coffee'
-    ])
-    .pipe(
-      gulpif(/[.]coffee$/,
-        coffee({bare: true})
-        .on('error', gutil.log)
-      )
-    )
-    .pipe(gulp.dest(buildDir))
+    // Compile JS test files. Not compiled.
+    var js = gulp.src(['./app/**/*_test.js'])
+        .pipe(sourcemaps.init());
+
+    var csJs = gulp.src(['./app/**/*_test.coffee'])
+        .pipe(sourcemaps.init())
+        .pipe(coffee({bare: true}).on('error', gutil.log));
+
+    es.merge(js, csJs)
+    .pipe(sourcemaps.write('/sourcemaps'))
+    .pipe(gulp.dest(buildDir + '/js'))
 });
 
 gulp.task('templates', function() {
@@ -60,7 +58,7 @@ gulp.task('templates', function() {
 
     es.merge(jadeTempl, htmlTempl, markdownTempl) //.pipe(gulpif(/[.]jade$/, jade({pretty:true}).on('error', gutil.log))
       .pipe(tplCache('templates.js',{standalone:true}))
-      .pipe(gulp.dest(buildDir))
+      .pipe(gulp.dest(buildDir + '/js'))
 });
 
 gulp.task('chartData', function() {
@@ -72,48 +70,43 @@ gulp.task('chartData', function() {
 gulp.task('appCSS', function() {
   // concatenate compiled Less and CSS
   // into build/app.css
-  gulp
-    .src([
-      './app/**/*.less',
-      './app/**/*.css'
-    ])
-    .pipe(
-      gulpif(/[.]less$/,
-        less({
-          paths: [
-            './bower_components/bootstrap/less'
-          ]
-        })
-        .on('error', gutil.log))
-    )
-    .pipe(
-      concat('app.css')
-    )
-    .pipe(
-      gulp.dest(buildDir)
-    )
+    var cssSrc = gulp.src(['./app/**/*.css'])
+        .pipe(sourcemaps.init());
+    var lessSrc = gulp.src(['./app/**/*.less'])
+        .pipe(sourcemaps.init())
+        .pipe(less({paths: ['./bower_components/bootstrap/less']}).on('error', gutil.log));
+
+    es.merge(cssSrc, lessSrc)
+    .pipe(concat('app.css'))
+    .pipe(sourcemaps.write('/sourcemaps'))
+    .pipe(gulp.dest(buildDir + '/css'))
 });
 
 gulp.task('libJS', function() {
   gulp.src(mainBowerFiles({filter:/\.js$/}),{base:'bower_components'})
       .pipe(sourcemaps.init())
       .pipe(concat('vendor.js'))
-      .pipe(sourcemaps.write('/maps'))
-      .pipe(gulp.dest(buildDir))
+      .pipe(sourcemaps.write('/sourcemaps'))
+      .pipe(gulp.dest(buildDir + '/js'))
 });
+
 gulp.task('libCSS',
   function() {
   // concatenate vendor css into build/lib.css
   gulp.src(mainBowerFiles({filter:/\.css$/}))
       .pipe(sourcemaps.init())
       .pipe(concat('lib.css'))
-      .pipe(sourcemaps.write('/maps'))
-      .pipe(gulp.dest(buildDir));
+      .pipe(sourcemaps.write('/sourcemaps'))
+      .pipe(gulp.dest(buildDir + '/css'));
 });
 
 gulp.task('index', function() {
-  gulp.src(['./app/index.jade', './app/index.html'])
-    .pipe(gulpif(/[.]jade$/, jade().on('error', gutil.log)))
+    var indexHtml = gulp.src(['./app/index.html']);
+
+    var indexJade = gulp.src(['./app/index.jade'])
+        .pipe(jade().on('error', gutil.log));
+
+    es.merge(indexHtml, indexJade)
     .pipe(gulp.dest(buildDir));
 });
 
