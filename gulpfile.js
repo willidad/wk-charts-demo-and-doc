cbd862ab696c018bf56850406851cc4cc54ab481
Willi Klenk <willi@klenk-bondorf.de>
2015-01-18 23:33:02 +0100

    fixed watcher path definition: remove ./ relative path so new and deleted files are detected

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 // file name and directory definitions
 
-var buildDir = './build';
-var heroku = './../heroku';
+var buildDir = 'build';
+var heroku = '../heroku';
 var libDir = './../wk-charts-build';
 
 var gulp            = require('gulp');
@@ -27,6 +27,7 @@ var annotate        = require('gulp-ng-annotate');
 var uglify          = require('gulp-uglify');
 var minifycss       = require('gulp-minify-css');
 var minifyhtml      = require('gulp-minify-html');
+var path            = require('path');
 
 gulp.task('bower-update', function() {
     return bower({ cmd: 'update'});
@@ -47,25 +48,25 @@ gulp.task('cleanBuildDir', function(done) {
 gulp.task('appJS', function() {
     // concatenate compiled .coffee files and js files
     // into build/app.js
-    var js = gulp.src(['!./app/**/*_test.js','./app/**/*.js'],{base:'./'})
+    var js = gulp.src(['!app/**/*_test.js','app/**/*.js'],{base:''})
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init());
 
-    var csJs = gulp.src(['!./app/**/*_test.coffee','./app/**/*.coffee'],{base:'./'})
+    var csJs = gulp.src(['!app/**/*_test.coffee','app/**/*.coffee'],{base:''})
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init())
         .pipe(coffee({bare:true, doctype:'html'}));
 
     return es.merge(js,csJs)
         .pipe(concat('app.js'))
-        .pipe(sourcemaps.write('./maps'))
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest(buildDir + '/js'))
         .pipe(lifereload())
         .pipe(notify({onLast:true, message:'App JS Build complete'}))
 });
 
 gulp.task('wkChartsCopyJs', function() {
-    return gulp.src([libDir + '/lib/wk-charts.js', libDir + '/lib/**/*.map'])
+    return gulp.src([path.join(libDir, '/lib/wk-charts.js'), path.join(libDir, '/lib/**/*.map')])
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(gulp.dest(buildDir + '/lib'))
         .pipe(lifereload())
@@ -73,7 +74,7 @@ gulp.task('wkChartsCopyJs', function() {
 });
 
 gulp.task('wkChartsCopyCss', function() {
-    return gulp.src([libDir + '/lib/wk-charts.css'])
+    return gulp.src([path.join(libDir, '/lib/wk-charts.css')])
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(gulp.dest(buildDir + '/lib'))
         .pipe(lifereload())
@@ -81,18 +82,18 @@ gulp.task('wkChartsCopyCss', function() {
 });
 
 gulp.task('wkChartsCopyDocs', function() {
-    return gulp.src([libDir + '/docs/**/*.*'])
+    return gulp.src([path.join(libDir, '/docs/**/*.*')])
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(gulp.dest(buildDir + '/lib/docs'))
 });
 
 gulp.task('testJS', function() {
     // Compile JS test files. Not compiled.
-    var js = gulp.src(['./app/**/*_test.js'],{base:'./'})
+    var js = gulp.src(['app/**/*_test.js'],{base:''})
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init());
 
-    var csJs = gulp.src(['./app/**/*_test.coffee'],{base:'./'})
+    var csJs = gulp.src(['app/**/*_test.coffee'],{base:''})
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init())
         .pipe(coffee({bare: true}).on('error', gutil.log));
@@ -106,16 +107,16 @@ gulp.task('templates', function() {
     // combine compiled Jade and html template files into
     // build/template.js
     // jade templates
-    var jadeTempl = gulp.src(['!./app/index.jade', './app/**/*.jade'])
+    var jadeTempl = gulp.src(['!app/index.jade', 'app/**/*.jade'])
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init())
         .pipe(jade({pretty:true, doctype:'html'}));
     // html templates
-    var htmlTempl = gulp.src(['!./app.index.html', './app/**/*.html'])
+    var htmlTempl = gulp.src(['!app.index.html', 'app/**/*.html'])
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init());
     // markdown templates
-    var markdownTempl = gulp.src(['./app/**/*.md'])
+    var markdownTempl = gulp.src(['app/**/*.md'])
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init())
         .pipe(markdown());
@@ -124,49 +125,49 @@ gulp.task('templates', function() {
         .pipe(tplCache({standalone:true}))
         //.pipe(annotate())
         //.pipe(uglify())
-        .pipe(sourcemaps.write('./maps'))
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest(buildDir + '/js'))
         .pipe(lifereload())
         .pipe(notify({onLast:true, message:'App Template Build complete'}))
 });
 
 gulp.task('chartData', function() {
-    return gulp.src(['./app/pages/**/data/*'])
+    return gulp.src(['app/pages/**/data/*'])
         .pipe(plumber({errorHandler: errorAlert}))
-        .pipe(gulp.dest('./build/data/pages'))
+        .pipe(gulp.dest('build/data/pages'))
 });
 
 gulp.task('appCSS', function() {
     // concatenate compiled Less and CSS
     // into build/app.css
-    var cssSrc = gulp.src(['./app/**/*.css'],{base:'./'})
+    var cssSrc = gulp.src(['app/**/*.css'],{base:''})
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init());
-    var lessSrc = gulp.src(['./app/**/*.less'],{base:'./'})
+    var lessSrc = gulp.src(['app/**/*.less'],{base:''})
         .pipe(sourcemaps.init())
         .pipe(plumber({errorHandler: errorAlert}))
-        .pipe(less({paths: ['./bower_components/bootstrap1/less']}).on('error', notify));
+        .pipe(less({paths: ['bower_components/bootstrap1/less']}).on('error', notify));
 
     return es.merge(cssSrc, lessSrc)
         .pipe(concat('app.css'))
         .pipe(minifycss())
-        .pipe(sourcemaps.write('./maps'))
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest(buildDir + '/css'))
         .pipe(lifereload())
 });
 
 gulp.task('libJS', function() {
-    return gulp.src(mainBowerFiles({filter:/\.js$/}),{base:'./'})
+    return gulp.src(mainBowerFiles({filter:/\.js$/}),{base:''})
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(sourcemaps.init())
         .pipe(concat('vendor.js'))
-        .pipe(sourcemaps.write('./maps'))
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest(buildDir + '/js'))
 });
 
 gulp.task('libCSS', function() {
     // concatenate vendor css into build/lib.css
-    return gulp.src(mainBowerFiles({filter:/\.css$/}),{base:'./'})
+    return gulp.src(mainBowerFiles({filter:/\.css$/}),{base:''})
         .pipe(sourcemaps.init())
         .pipe(concat('lib.css'))
         .pipe(sourcemaps.write())
@@ -174,9 +175,9 @@ gulp.task('libCSS', function() {
 });
 
 gulp.task('index', function() {
-    var indexHtml = gulp.src(['./app/index.html']);
+    var indexHtml = gulp.src(['app/index.html']);
 
-    var indexJade = gulp.src(['./app/index.jade'])
+    var indexJade = gulp.src(['app/index.jade'])
         .pipe(plumber({errorHandler: errorAlert}))
         .pipe(jade({doctype:'html'}));
 
@@ -189,15 +190,15 @@ gulp.task('index', function() {
 gulp.task('watch',function() {
     // watch files to build
     lifereload.listen({basePath:buildDir});
-    gulp.watch(['./app/**/*.coffee', '!./app/**/*_test.coffee', './app/**/*.js', '!./app/**/*_test.js'], ['appJS']); // application source
-    gulp.watch(['./app/**/*_test.coffee', './app/**/*_test.js'], ['testJS']); // application tests
-    gulp.watch(['!./app/index.jade', '!./app/index.html', './app/**/*.jade', './app/**/*.html', './app/**/*.md'], ['templates']); //application templates
-    gulp.watch(['./app/**/*.less', './app/**/*.css'], ['appCSS']); // application css
-    gulp.watch(['./app/index.jade', './app/index.html'], ['index']); // index file
-    gulp.watch(['./app/pages/**/data/*.csv'], ['chartData']); // examples data files
-    gulp.watch([libDir + '/lib/wk-charts.js'], ['wkChartsCopyJs']); // copy chart library on change
-    gulp.watch([libDir + '/lib/wk-charts.css'], ['wkChartsCopyCss']); // copy chart css on change
-    gulp.watch([libDir + '/dist/docs/**/*.html'], ['wkChartsCopyDocs']); // copy chart docs on change
+    gulp.watch(['app/**/*.coffee', '!app/**/*_test.coffee', 'app/**/*.js', '!app/**/*_test.js'], ['appJS']); // application source
+    gulp.watch(['app/**/*_test.coffee', 'app/**/*_test.js'], ['testJS']); // application tests
+    gulp.watch(['!app/index.jade', '!app/index.html', 'app/**/*.jade', 'app/**/*.html', 'app/**/*.md'], ['templates']); //application templates
+    gulp.watch(['app/**/*.less', 'app/**/*.css'], ['appCSS']); // application css
+    gulp.watch(['app/index.jade', 'app/index.html'], ['index']); // index file
+    gulp.watch(['app/pages/**/data/*.csv'], ['chartData']); // examples data files
+    gulp.watch([path.join(libDir,'/lib/wk-charts.js')], ['wkChartsCopyJs']); // copy chart library on change
+    gulp.watch([path.join(libDir,'/lib/wk-charts.css')], ['wkChartsCopyCss']); // copy chart css on change
+    gulp.watch([path.join(libDir,'/dist/docs/**/*.html')], ['wkChartsCopyDocs']); // copy chart docs on change
 });
 
 gulp.task('default', function(done) {
